01e20d140561d4678dd1de935c1fd2e4924df014
tomsnep <Snep@023.121.028.145.hva.nl>
2016-02-08 11:44:52 +0100

    fixed map structure

---
+++ week 2/exercise 1/static/js/app.js
@@ -0,0 +1,50 @@
+// add iife
+(function() {
+	'use strict'
+
+	var app = {
+		init: function(){
+
+			// call routes
+			routes.init();
+		}
+	};
+
+	var routes = {
+		
+		init: function(){
+			
+			// use available hash to save state otherwise use #startscreen as default
+			var currentHash = window.location.hash || '#startscreen';
+			// new var for caching
+			var currentHashEl = document.querySelector(currentHash);
+			currentHashEl.classList.add('active');
+
+			window.addEventListener('hashchange', function(){
+				
+				// declare newHash var with the hash
+				var newHash = window.location.hash;
+
+				// call sections.toggle()
+				sections.toggle(currentHash,newHash);
+
+				//update currentHash with the newHash
+				currentHash = newHash;
+			}, false);
+		}
+	};
+
+	var sections = {
+		toggle: function(currentHash,newHash){
+
+			var currentEl = document.querySelector(currentHash);
+			var newEl = document.querySelector(newHash);
+
+			currentEl.classList.remove('active');
+			newEl.classList.add('active');
+
+		}
+	};
+
+	app.init(); // call app.init()
+})();
